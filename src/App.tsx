054419d05c1d53a3f526747dfe632eb172320692
Louis Grao <louisgrao@gmail.com>
2026-02-10 16:52:11 +0000

    feat: add tailwind and learned useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
-const user = {
-  name: "brian",
-  age: 53,
-  occupation: "egg and spoon race adjudicator",
-  image: "src/assets/brian.png",
-};
+const user = [
+  {
+    name: "Brian",
+    age: 53,
+    occupation: "Egg and spoon race adjudicator",
+    image: "src/assets/brian.png",
+  },
+  {
+    name: "Bob",
+    age: 38,
+    occupation: "Turtle tamer",
+    image: "src/assets/bob.png",
+  },
+  {
+    name: "Billy",
+    age: 99,
+    occupation: "Accountant",
+    image: "src/assets/billy.png",
+  },
+];
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+  function Button() {
+    function nextUser() {
+      setIndex((index + 1) % user.length);
+    }
+
+    return (
+      <>
+        <button onClick={nextUser}>Next</button>
+      </>
+    );
+  }
+
   return (
     <>
       <div>
-        <img src={user.image} alt={"image of " + user.name} width={150} />
-        <p>{"Name: " + user.name}</p>
-        <p>{"Age: " + user.age}</p>
-        <p>{"Occupation: " + user.occupation}</p>
+        <img
+          src={user[index].image}
+          alt={"image of " + user[index].name}
+          width={150}
+        />
+        <p className="text-3xl font-bold underline">
+          {"Name: " + user[index].name}
+        </p>
+        <p>{"Age: " + user[index].age}</p>
+        <p>{"Occupation: " + user[index].occupation}</p>
+        <Button />
       </div>
     </>
   );
